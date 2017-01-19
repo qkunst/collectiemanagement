4ae25ea399a5c2c7c64f877fd696f643462494d2
Maarten Brouwers <github.com@murb.nl>
2017-01-19 17:07:02 +0100

    disable serviceworkers

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -158,17 +158,17 @@ $(document).on("click keydown", "button[method=post]", function(e) {
   var form = $(e.target).parents("form[data-auto-submit=true]");
   form.attr("method","post")
 });
-
-if ('serviceWorker' in navigator) {
-  navigator.serviceWorker.register('/sw.js', {scope: '/'})
-  .then(function(reg) {
-    // registration worked
-    console.log('Registration succeeded. Scope is ' + reg.scope);
-  }).catch(function(error) {
-    // registration failed
-    console.log('Registration failed with ' + error);
-  });
-}
+//
+// if ('serviceWorker' in navigator) {
+//   navigator.serviceWorker.register('/sw.js', {scope: '/'})
+//   .then(function(reg) {
+//     // registration worked
+//     console.log('Registration succeeded. Scope is ' + reg.scope);
+//   }).catch(function(error) {
+//     // registration failed
+//     console.log('Registration failed with ' + error);
+//   });
+// }
 
 // f = FormStore.Form.parseForm(document.forms[0])
 // f.submitForm()
