b5c780457ef4d129abb4850ee3a23b8e65433944
Maarten Brouwers <github.com@murb.nl>
2017-01-24 16:42:48 +0100

    always return an enum

--- app/models/geoname_summary.rb
+++ app/models/geoname_summary.rb
@@ -35,7 +35,7 @@ class GeonameSummary < ApplicationRecord
 
       # results = self.selectable.where(arel_table[:name].matches("%#{name}%"))
       # return results.sort if results.count > 1
-      # return []
+      return []
     end
     def to_array
       self.all.collect{|a|  {id: a.geoname_id, name: a.name, desc: a.parent_description }}
