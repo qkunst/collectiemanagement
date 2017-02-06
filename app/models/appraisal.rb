a17fde56af89a3d8b6a752740fbf5d6f5dd62f49
Maarten Brouwers <github.com@murb.nl>
2017-02-06 17:47:03 +0100

    added additional rspec

--- app/models/appraisal.rb
+++ app/models/appraisal.rb
@@ -2,11 +2,13 @@ class Appraisal < ApplicationRecord
   belongs_to :work
   belongs_to :user
 
+  validates_presence_of :appraised_on
+
   include ActionView::Helpers::NumberHelper
 
   scope :descending_appraisal_on, -> { order("appraisals.appraised_on is null, appraisals.appraised_on desc") }
 
   def name
-    "#{I18n.l appraised_on} (by #{appraised_by}): MW #{number_to_currency(market_value)}; VW #{number_to_currency(replacement_value)}"
+    "#{appraised_on ? I18n.l(appraised_on) : 'onbekende datum'} (by #{appraised_by}): MW #{number_to_currency(market_value)}; VW #{number_to_currency(replacement_value)}"
   end
 end
--- spec/models/appraisal_spec.rb
+++ spec/models/appraisal_spec.rb
@@ -6,6 +6,9 @@ RSpec.describe Appraisal, type: :model do
       it "should return a valid name" do
         expect(appraisals(:appraisal1).name).to eq("01-01-2000 (by test): MW €1.000,00; VW €2.000,00")
       end
+      it "should also work when date is not present" do
+        expect(appraisals(:appraisal_without_date).name).to eq("onbekende datum (by test): MW €1.000,00; VW €2.000,00")
+      end
     end
   end
 end
