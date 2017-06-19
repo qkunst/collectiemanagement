0100b8358a885a41a0750693e1e9ecda1ac9db35
Maarten Brouwers <github.com@murb.nl>
2017-06-19 23:01:06 +0200

    2.4.1

--- config/deploy.rb
+++ config/deploy.rb
@@ -38,7 +38,7 @@ set :linked_dirs, %w{log tmp public/uploads storage}
 
 # rbenv
 set :rbenv_type, :user
-set :rbenv_ruby, '2.3.3'
+set :rbenv_ruby, '2.4.1'
 set :rbenv_prefix, "RBENV_ROOT=#{fetch(:rbenv_path)} RBENV_VERSION=#{fetch(:rbenv_ruby)} #{fetch(:rbenv_path)}/bin/rbenv exec"
 set :rbenv_map_bins, %w{rake gem bundle ruby rails}
 set :rbenv_roles, :all
