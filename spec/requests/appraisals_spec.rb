14779576afb6bf57b65476a73c241204203f2211
Maarten Brouwers <github.com@murb.nl>
2017-06-20 21:31:01 +0200

    edit requests appraisal spec

--- spec/requests/appraisals_spec.rb
+++ spec/requests/appraisals_spec.rb
@@ -22,4 +22,25 @@ RSpec.describe "Appraisals", type: :request do
       expect(response).to have_http_status(200)
     end
   end
+  describe "GET /collections/1/works/1/appraisals/1/edit" do
+    it "it refuses by default" do
+      work = works(:work1)
+      get edit_collection_work_appraisal_path(work.collection, work, appraisals(:appraisal4))
+      expect(response).to have_http_status(302)
+    end
+    it "it refuses by qkunst" do
+      work = works(:work1)
+      user = users(:qkunst)
+      sign_in user
+      get edit_collection_work_appraisal_path(work.collection, work, appraisals(:appraisal4))
+      expect(response).to have_http_status(302)
+    end
+    it "it works for admin" do
+      work = works(:work1)
+      user = users(:admin)
+      sign_in user
+      get edit_collection_work_appraisal_path(work.collection, work, appraisals(:appraisal4))
+      expect(response).to have_http_status(200)
+    end
+  end
 end
