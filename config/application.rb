9f01cd242cf0b92895daa9fdd6b644422782825c
Maarten Brouwers <github.com@murb.nl>
2018-01-15 23:28:14 +0100

    headersfilter readded

--- config/application.rb
+++ config/application.rb
@@ -14,7 +14,7 @@ module SourceQkunstbeheer
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
-    # config.middleware.insert_before(0, Rack::HeadersFilter)
+    config.middleware.insert_before(0, Rack::HeadersFilter)
   end
 end
 
