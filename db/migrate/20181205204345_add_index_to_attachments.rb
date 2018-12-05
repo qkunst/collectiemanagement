d5f0aa882b2fb721e19a704e320bba523168e4ec
Maarten Brouwers <github.com@murb.nl>
2018-12-05 21:46:40 +0100

    add better index on attachments

---
+++ db/migrate/20181205204345_add_index_to_attachments.rb
@@ -0,0 +1,5 @@
+class AddIndexToAttachments < ActiveRecord::Migration[5.2]
+  def change
+    add_index :attachments, [:attache_id, :attache_type]
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_12_05_203708) do
+ActiveRecord::Schema.define(version: 2018_12_05_204345) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -73,6 +73,7 @@ ActiveRecord::Schema.define(version: 2018_12_05_203708) do
     t.string "visibility"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["attache_id", "attache_type"], name: "index_attachments_on_attache_id_and_attache_type"
     t.index ["attache_id"], name: "index_attachments_on_attache_id"
   end
 
