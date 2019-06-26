8da88e1c399cdf5f19ce7ce74afad7a67645777e
Maarten Brouwers <github.com@murb.nl>
2019-06-26 21:46:33 +0200

    sidekiq check

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ require 'sidekiq/web'
 
 Rails.application.routes.draw do
 
+  get 'application_status' => "status#application_status"
   # config/routes.rb
   authenticate :user, lambda { |u| u.admin? } do
     mount Sidekiq::Web => '/sidekiq'
