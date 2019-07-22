cca41f02b0ce6dc3a93ded8fe1ae7e351c19b3bc
Maarten Brouwers <github.com@murb.nl>
2019-07-22 09:44:12 +0200

    worker may simply be done for the day

--- app/controllers/status_controller.rb
+++ app/controllers/status_controller.rb
@@ -1,6 +1,10 @@
 class StatusController < ApplicationController
   def application_status
     @sidekiq_running = Sidekiq::Workers.new.size > 0
+    if @sidekiq_running == false
+      IndexWorkWorker.perform_async( Work.ids.sample )
+      @sidekiq_running = Sidekiq::Workers.new.size > 0
+    end
     begin
       Collection.first.search_works("testsearch")[0]
       @elastic_search_running = true
