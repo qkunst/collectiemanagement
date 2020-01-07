998eddf17d0ee92f97147deb616094270c905dc3
Maarten Brouwers <github.com@murb.nl>
2020-01-07 23:40:31 +0100

    fix: root colletion reference wasn't dynamic anymore

--- app/models/collection/hierarchy.rb
+++ app/models/collection/hierarchy.rb
@@ -33,7 +33,7 @@ module Collection::Hierarchy
     def expand_with_parent_collections
       self.id ? Collection.unscope(:order).joins("INNER JOIN (
         SELECT CAST(regexp_split_to_table(branch,'~') AS INTEGER) AS branch_split, branch
-          FROM connectby('collections', 'id', 'parent_collection_id', '9', 0, '~') as (id int, pid int, lvl int, branch text)
+          FROM connectby('collections', 'id', 'parent_collection_id', '#{Collection.unscoped.root_collection.id}', 0, '~') as (id int, pid int, lvl int, branch text)
           WHERE id = #{self.id}
         ) AS collection_branche_ids ON collections.id = collection_branche_ids.branch_split") : Collection.none
     end
