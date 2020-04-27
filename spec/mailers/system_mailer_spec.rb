eb9f74c7db83cfd809c590deb25e771eac74192e
Maarten Brouwers <github.com@murb.nl>
2020-04-27 10:46:10 +0200

    naam en logo aanpassing

--- spec/mailers/system_mailer_spec.rb
+++ spec/mailers/system_mailer_spec.rb
@@ -7,7 +7,7 @@ RSpec.describe SystemMailer, type: :mailer do
     it "renders the headers" do
       expect(mail.subject).to eq("StandardError fout in QKunst Collectiebeheer")
       expect(mail.to).to eq(["qkunst@murb.nl"])
-      expect(mail.from).to eq(["collectiebeheer@qkunst.nl"])
+      expect(mail.from).to eq(["collectiemanagement@qkunst.nl"])
     end
 
     it "renders the body" do
