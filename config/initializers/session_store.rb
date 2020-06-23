66d27d626bc9845526d3a099198f119d380fb5a4
Maarten Brouwers <github.com@murb.nl>
2020-06-23 18:11:50 +0200

    stricter cookie config

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -2,4 +2,4 @@
 
 # Be sure to restart your server when you modify this file.
 
-Rails.application.config.session_store :active_record_store, key: "_source_qkunstbeheer_session"
+Rails.application.config.session_store :active_record_store, key: '__Host-collectiemanagement_session', same_site: :strict, secure: Rails.env.production?
