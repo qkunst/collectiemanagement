b378fe0955da5d5da1fb900a01486cc1eabe7b6e
Maarten Brouwers <github.com@murb.nl>
2020-06-23 17:52:51 +0200

    stricter csp

--- config/initializers/content_security_policy.rb
+++ config/initializers/content_security_policy.rb
@@ -9,12 +9,12 @@
 Rails.application.config.content_security_policy do |policy|
   policy.connect_src :self, :https, "http://localhost:3035", "ws://localhost:3035" if Rails.env.development?
 
-  policy.default_src :self, :https
-  policy.font_src    :self, :https, :data
-  policy.img_src     :self, :https, :data
+  policy.default_src :self
+  policy.font_src    :self, :data
+  policy.img_src     :self, :data
   policy.object_src  :none
-  policy.script_src  :self, :https
-  policy.style_src   :self, :https
+  policy.script_src  :self
+  policy.style_src   :self
 
 #   # Specify URI for violation reports
 #   # policy.report_uri "/csp-violation-report-endpoint"
