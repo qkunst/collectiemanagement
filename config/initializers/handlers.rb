059de52e36e357d0cc891dcc111cba3cdbe29e39
Maarten Brouwers <github.com@murb.nl>
2020-11-16 16:40:27 +0100

    Revert "drop markdown handling"
    This reverts commit ea0eb7acda1f700654c97b13060812a5b35f791a.

--- config/initializers/handlers.rb
+++ config/initializers/handlers.rb
@@ -1,5 +1,5 @@
 require "#{Rails.root}/app/handlers/nokogiri_handler"
-# require "#{Rails.root}/app/handlers/markdown_handler"
+require "#{Rails.root}/app/handlers/markdown_handler"
 
 ActionView::Template.register_template_handler :nokogiri, Handlers::NokogiriHandler.new
-# ActionView::Template.register_template_handler :md, Handlers::MarkdownHandler.new
+ActionView::Template.register_template_handler :md, Handlers::MarkdownHandler.new
