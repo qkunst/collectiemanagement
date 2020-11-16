5693984ae4803cc2f72cf5f9ef5500393809d3db
Maarten Brouwers <github.com@murb.nl>
2020-11-16 16:22:27 +0100

    cache store to default

--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -59,7 +59,7 @@ Rails.application.configure do
   config.log_tags = [:request_id]
 
   # Use a different cache store in production.
-  config.cache_store = :redis_store, "redis://localhost:6379/0/cache", {expires_in: 1.week}
+  # config.cache_store = :redis_store, "redis://localhost:6379/0/cache", {expires_in: 1.week}
 
   # Use a real queuing backend for Active Job (and separate queues per environment)
   # config.active_job.queue_adapter     = :sidekiq
