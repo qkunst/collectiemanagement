fe7feab31834a6dcaf884bba03ac9274dce4da62
Maarten Brouwers <github.com@murb.nl>
2020-11-18 00:31:08 +0100

    include balance reporting and use symbols as keys

--- app/models/report/builder.rb
+++ app/models/report/builder.rb
@@ -63,7 +63,7 @@ module Report
           #
         }
 
-        [:subset, :style, :frame_type].each do |key|
+        [:subset, :style, :frame_type, :balance_category].each do |key|
           aggregation.merge!(basic_aggregation_snippet(key, "_id"))
         end
 
@@ -75,25 +75,36 @@ module Report
           aggregation.merge!(basic_aggregation_snippet(key, ".id"))
         end
 
-        ["abstract_or_figurative", "grade_within_collection", "object_format_code", "tag_list", :market_value, :market_value_min, :market_value_max, :replacement_value, :replacement_value_min, :replacement_value_max, :object_creation_year, :purchase_year, :purchase_price_in_eur, :minimum_bid, :selling_price, :publish, :image_rights].each do |key|
+        ["abstract_or_figurative", "grade_within_collection", "object_format_code", "tag_list", :object_creation_year, :purchase_year, :purchase_price_in_eur, :minimum_bid, :selling_price, :publish, :image_rights].each do |key|
           aggregation.merge!(basic_aggregation_snippet_with_missing(key))
+        end
+
+        [:market_value, :replacement_value].each do |key|
+          aggregation.merge!(basic_aggregation_snippet_with_missing(key))
+          # aggregation.merge!(basic_aggregation_snippet(key, "", key, -1))
         end
 
         [:inventoried, :refound, :new_found].each do |key|
           aggregation.merge!(basic_aggregation_snippet(key))
         end
 
-        market_value_range = basic_aggregation_snippet_with_missing(:market_value_range, "", :market_value_min)
+
+        aggregation[:market_value_missing][:aggs] = basic_aggregation_snippet(:balance_category, "_id")
+        aggregation[:market_value_missing][:aggs].merge!({market_value_range_missing: { missing: { field: :market_value_max }}})
+        aggregation[:replacement_value_missing][:aggs] = basic_aggregation_snippet(:balance_category, "_id")
+        aggregation[:replacement_value_missing][:aggs].merge!({replacement_value_range_missing: { missing: { field: :market_value_max }}})
+
+        market_value_range = basic_aggregation_snippet(:market_value_range, "", :market_value_min)
         market_value_range[:market_value_range][:aggs] = basic_aggregation_snippet(:market_value_max)
         aggregation.merge!(market_value_range)
 
-        replacement_value_range = basic_aggregation_snippet_with_missing(:replacement_value_range, "", :replacement_value_min)
+        replacement_value_range = basic_aggregation_snippet(:replacement_value_range, "", :replacement_value_min)
         replacement_value_range[:replacement_value_range][:aggs] = basic_aggregation_snippet(:replacement_value_max)
         aggregation.merge!(replacement_value_range)
 
-        location_sub_sub = basic_aggregation_snippet_with_missing("location_detail_raw")
+        location_sub_sub = basic_aggregation_snippet_with_missing(:location_detail_raw)
 
-        location_sub = basic_aggregation_snippet_with_missing("location_floor_raw")
+        location_sub = basic_aggregation_snippet_with_missing(:location_floor_raw)
         location_sub.keys.each do |key|
           location_sub[key][:aggs] = location_sub_sub
         end
--- app/models/report/builder_helpers.rb
+++ app/models/report/builder_helpers.rb
@@ -2,19 +2,21 @@
 
 module Report
   module BuilderHelpers
-    def basic_aggregation_snippet key, postfix = "", field = nil
-      {
-        key => {
+    def basic_aggregation_snippet key, postfix = "", field = nil, missing = nil
+      rv = {
+        key.to_sym => {
           terms: {
             field: (field || "#{key}#{postfix}"), size: 999
           }
         }
       }
+      rv[key.to_sym][:terms][:missing] = missing if missing
+      rv
     end
 
     def basic_aggregation_snippet_with_missing key, postfix = "", field = nil
       rv = basic_aggregation_snippet(key, postfix, field)
-      rv["#{key}_missing"] = {
+      rv["#{key}_missing".to_sym] = {
         missing: {
           field: (field || "#{key}#{postfix}")
         }
