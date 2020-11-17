136df56f9c6b29b1a8b08185a240c1f72420c3a6
Maarten Brouwers <github.com@murb.nl>
2020-11-18 00:31:40 +0100

    allow for aggregations within missing

--- app/models/report/parser_helpers.rb
+++ app/models/report/parser_helpers.rb
@@ -2,27 +2,35 @@
 
 module Report
   module ParserHelpers
+    IGNORE_KEYS = ["key", "doc_count", "total"]
+
+    def parse_bucket bucket
+      subcounts_in_hash = {}
+      bucket.each do |subkey, subset|
+        sub_counts = parse_aggregation(subset, subkey)
+        subkey = subkey.gsub(/_missing$/, "")
+        if sub_counts.present?
+          if subcounts_in_hash[subkey.to_sym]
+            subcounts_in_hash[subkey.to_sym].deep_merge! sub_counts
+          else
+            subcounts_in_hash[subkey.to_sym] = sub_counts
+          end
+        end
+      end
+      return subcounts_in_hash
+    end
+
     def parse_aggregation aggregation, aggregation_key
       counts = {}
       # raise aggregation
-      if aggregation.is_a?(Hash) && aggregation[:doc_count] && aggregation_key.to_s.match(/^.*_missing$/)
+      if aggregation.is_a?(Hash) && aggregation[:doc_count] && aggregation_key.to_s.match(/^.*_missing$/) &&  (aggregation.keys - IGNORE_KEYS).count == 0
         counts[:missing] = {count: aggregation[:doc_count], subs: {}}
       elsif aggregation.is_a?(Hash) && aggregation[:buckets]
         buckets = aggregation.buckets # .sort{|a,b| a[:key]<=>b[:key]}
         # raise buckets
         buckets.each do |bucket|
-          subcounts_in_hash = {}
-          bucket.each do |subkey, subset|
-            sub_counts = parse_aggregation(subset, subkey)
-            subkey = subkey.gsub(/_missing$/, "")
-            if sub_counts
-              if subcounts_in_hash[subkey.to_sym]
-                subcounts_in_hash[subkey.to_sym].deep_merge! sub_counts
-              else
-                subcounts_in_hash[subkey.to_sym] = sub_counts
-              end
-            end
-          end
+          subcounts_in_hash = parse_bucket(bucket)
+
           key = parse_bucket_key(aggregation_key, bucket["key"])
 
           key_model = key_model_relations[aggregation_key]
@@ -32,8 +40,16 @@ module Report
 
           counts[key] = {count: bucket.doc_count, subs: subcounts_in_hash}
         end
+      elsif aggregation.is_a?(Hash) && (aggregation.keys - IGNORE_KEYS).count > 0 && !IGNORE_KEYS.include?(aggregation_key)
+        subcounts_in_hash = parse_bucket aggregation
+
+        if aggregation_key.to_s.match(/^.*_missing$/)
+          aggregation_key = :missing
+        end
+
+        counts[aggregation_key] = {count: aggregation.doc_count, subs: subcounts_in_hash}
       end
-      return counts unless ["key", "doc_count", "total"].include?(aggregation_key)
+      return counts unless IGNORE_KEYS.include?(aggregation_key)
     end
 
     def parse_bucket_key aggregation_key, bucket_key
@@ -41,9 +57,6 @@ module Report
 
       unless ["abstract_or_figurative", "object_format_code", "grade_within_collection", "location_raw", "location_floor_raw", "location_detail_raw", "tag_list"].include?(aggregation_key)
         bucket_key_parsed = bucket_key.to_s.split(",").map(&:to_i)
-      end
-      if aggregation_key.match(/_value/) && bucket_key == -1.0
-        bucket_key_parsed = :missing
       end
       bucket_key_parsed
     end
