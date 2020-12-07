4e292ac55156cf344562ddb97dc5b836433bef6b
Maarten Brouwers <github.com@murb.nl>
2020-12-07 21:04:00 +0100

    work sets: additional integration test

---
+++ spec/features/user_can_create_a_work_set_spec.rb
@@ -0,0 +1,33 @@
+# frozen_string_literal: true
+
+require_relative "feature_helper"
+
+RSpec.feature "Werken groeperen", type: :feature do
+  include FeatureHelper
+
+  scenario "Advisor can group works" do
+    login "qkunst-test-advisor@murb.nl"
+
+    click_on "Collecties"
+    if page.body.match?("id=\"list-to-filter\"")
+      within "#list-to-filter" do
+        click_on "Collection 1"
+      end
+    end
+    click_on "Toon alle 5 werken"
+
+    work_to_edit1 = works(:work1)
+    work_to_edit2 = works(:work2)
+
+    save_and_open_page
+    check "selected_works_#{work_to_edit1.id}"
+    check "selected_works_#{work_to_edit2.id}"
+
+    click_on "Groepeer"
+
+    select "Meerluik"
+    fill_in "Identificatienummer", with: "123"
+    click_on "Werkgroepering toevoegen"
+    save_and_open_page
+  end
+end
