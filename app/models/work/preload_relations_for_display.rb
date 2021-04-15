2952bedb77b02f79681fb963a854b4cc4566b707
Maarten Brouwers <github.com@murb.nl>
2021-04-15 16:59:53 +0200

    download styling

--- app/models/work/preload_relations_for_display.rb
+++ app/models/work/preload_relations_for_display.rb
@@ -11,7 +11,7 @@ module Work::PreloadRelationsForDisplay
       when :limited, :limited_auction
         includes(:collection, :techniques, :object_categories, :medium, :condition_work, :condition_frame, artists: [:artist_involvements], work_sets: [:work_set_type])
       else
-        includes(:collection, :created_by, :library_items, :purchase_price_currency, :techniques, :object_categories, :damage_types, :frame_damage_types, :frame_type, :medium, :style, :themes, :subset, :sources, :owner, :work_status, :attachments, :appraisals, :condition_work, :condition_frame, :cluster, :placeability, :balance_category, :geoname_summary, artists: [:artist_involvements], work_sets: [:work_set_type, :appraisals])
+        includes(:collection, :created_by, :library_items, :purchase_price_currency, :techniques, :object_categories, :damage_types, :frame_damage_types, :frame_type, :medium, :style, :themes, :subset, :sources, :owner, :work_status, :attachments, :appraisals, :condition_work, :condition_frame, :cluster, :placeability, :balance_category, :work_set_types, :geoname_summary, artists: [:artist_involvements], work_sets: [:work_set_type, :appraisals])
       end
     end
   end
