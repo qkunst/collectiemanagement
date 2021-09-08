ba4fd4d6ad503387bf12d7d06fb0e6ce193aa39a
murb <git@murb.nl>
2021-09-08 12:12:21 +0200

    surface calc

---
+++ app/models/work/sizing_rendering.rb
@@ -0,0 +1,100 @@
+# frozen_string_literal: true
+
+module Work::SizingRendering
+  extend ActiveSupport::Concern
+  MAX_ACCEPTABLE_DEPTH_FOR_2D = 10 # 10cm
+
+  included do
+    def frame_size
+      whd_to_s(frame_width, frame_height, frame_depth, frame_diameter)
+    end
+
+    def frame_size_with_fallback
+      frame_size || work_size
+    end
+
+    def hpd_height
+      rv = frame_height? ? frame_height : height
+      rv if rv && (rv > 0)
+    end
+
+    def hpd_width
+      rv = frame_width? ? frame_width : width
+      rv if rv && (rv > 0)
+    end
+
+    def hpd_depth
+      rv = frame_depth? ? frame_depth : depth
+      rv if rv && (rv > 0)
+    end
+
+    def hpd_diameter
+      rv = frame_diameter? ? frame_diameter : diameter
+      rv if rv && (rv > 0)
+    end
+
+    def three_dimensional?
+      !!(hpd_depth && hpd_depth > MAX_ACCEPTABLE_DEPTH_FOR_2D || hpd_diameter)
+    end
+
+    def floor_bound?
+      three_dimensional?
+    end
+
+    # return in meters
+    def wall_surface
+      if !floor_bound? && hpd_width && hpd_height
+        (hpd_width * hpd_height) / 10000.0
+      end
+    end
+
+    # return in meters
+    def floor_surface
+      if floor_bound? && hpd_width && hpd_depth
+        (hpd_width * hpd_depth) / 10000.0
+      elsif floor_bound? && hpd_diameter
+        (hpd_diameter * hpd_diameter) / 10000.0
+      end
+    end
+
+    def surfaceless?
+      !floor_surface && !wall_surface
+    end
+
+    def object_format_code
+      size = [hpd_height, hpd_width, hpd_depth, hpd_diameter].compact.max
+      if !size
+      elsif size < 30
+        :xs
+      elsif size < 50
+        :s
+      elsif size < 80
+        :m
+      elsif size < 120
+        :l
+      elsif size >= 120
+        :xl
+      end
+    end
+
+    def whd_to_s width = nil, height = nil, depth = nil, diameter = nil
+      whd_values = [width, height, depth].collect { |a| dimension_to_s(a) }.compact
+      rv = whd_values.join(" × ")
+      if whd_values.count > 0
+        legend = []
+        legend << "b" unless width.to_s == ""
+        legend << "h" unless height.to_s == ""
+        legend << "d" unless depth.to_s == ""
+        rv = "#{rv} (#{legend.join("×")})"
+      end
+      rv = [rv, "⌀ #{dimension_to_s(diameter)}"].compact.join("; ") if dimension_to_s(diameter)
+      return nil if rv.empty?
+      rv
+    end
+
+    def work_size
+      whd_to_s(width, height, depth, diameter)
+    end
+
+  end
+end
---
+++ spec/models/work/sizing_rendering_spec.rb
@@ -0,0 +1,79 @@
+require_relative "../../rails_helper"
+
+RSpec.describe Work::SizingRendering do
+  describe "#three_dimensional?" do
+    it "returns false for an empty work" do
+      expect(Work.new.three_dimensional?).to eq(false)
+    end
+
+    it "returns false for a 2d work" do
+      expect(Work.new(width: 20, height: 20).three_dimensional?).to eq(false)
+    end
+
+    it "returns false for a 2d work with a 9cm deep frame" do
+      expect(Work.new(width: 20, height: 20, frame_depth: 9).three_dimensional?).to eq(false)
+    end
+
+    it "returns false for a 3d work" do
+      expect(Work.new(width: 20, height: 20, frame_depth: 19).three_dimensional?).to eq(true)
+      expect(Work.new(frame_width: 20, frame_height: 20, frame_diameter: 19).floor_bound?).to eq(true)
+    end
+  end
+
+  describe "#floor_bound?" do
+    it "returns false for an empty work" do
+      expect(Work.new.floor_bound?).to eq(false)
+    end
+
+    it "returns false for a 2d work" do
+      expect(Work.new(width: 20, height: 20).floor_bound?).to eq(false)
+    end
+
+    it "returns false for a 2d work with a 9cm deep frame" do
+      expect(Work.new(width: 20, height: 20, frame_depth: 9).floor_bound?).to eq(false)
+    end
+
+    it "returns false for a 3d work" do
+      expect(Work.new(width: 20, height: 20, frame_depth: 19).floor_bound?).to eq(true)
+      expect(Work.new(width: 20, height: 20, frame_diameter: 19).floor_bound?).to eq(true)
+    end
+  end
+
+  describe "#wall_surface" do
+    it "returns false for an empty work" do
+      expect(Work.new.wall_surface).to eq(nil)
+    end
+
+    it "returns square meter for a 2d work" do
+      expect(Work.new(width: 20, height: 20).wall_surface).to eq(0.04)
+    end
+
+    it "returns square meter for a 2d work" do
+      expect(Work.new(width: 100, height: 100).wall_surface).to eq(1)
+    end
+
+    it "returns nil for a 3d work" do
+      expect(Work.new(width: 20, height: 20, frame_depth: 19).wall_surface).to eq(nil)
+      expect(Work.new(width: 20, height: 20, frame_diameter: 19).wall_surface).to eq(nil)
+    end
+  end
+
+  describe "#wall_surface" do
+    it "returns nil for an empty work" do
+      expect(Work.new.floor_surface).to eq(nil)
+    end
+
+    it "returns nil  for a 2d work" do
+      expect(Work.new(width: 20, height: 20).floor_surface).to eq(nil)
+    end
+
+    it "returns values for a 3d work" do
+      expect(Work.new(width: 20, height: 20, frame_depth: 40).floor_surface).to eq(0.08)
+      expect(Work.new(width: 100, height: 2, frame_diameter: 100).floor_surface).to eq(1)
+    end
+  end
+
+  describe "#surfaceless?" do
+
+  end
+end
