1f401e8157553251820c8411e1680e06f66e41b3
murb <git@murb.nl>
2021-12-08 13:06:25 +0100

    add limit parameter to api

--- app/controllers/api/v1/works_controller.rb
+++ app/controllers/api/v1/works_controller.rb
@@ -6,7 +6,9 @@ class Api::V1::WorksController < Api::V1::ApiController
   def index
     @collection = @user.accessible_collections.find(params[:collection_id])
     api_authorize! :read_api, @collection
-    @works = @collection.works_including_child_works.all
+    @works = @collection.works_including_child_works
+    @works = @works.limit(params[:limit].to_i) if params[:limit]
+    @works = @works.all
   end
 
   def show
