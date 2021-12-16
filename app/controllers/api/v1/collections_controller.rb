ac782094851ec93538008a0885c4e806f31779c9
murb <git@murb.nl>
2021-12-16 16:28:10 +0100

    allow for listing *all* accessible collections

--- app/controllers/api/v1/collections_controller.rb
+++ app/controllers/api/v1/collections_controller.rb
@@ -1,6 +1,6 @@
 class Api::V1::CollectionsController < Api::V1::ApiController
   before_action :authenticate_activated_user!
   def index
-    @collections = @user.collections
+    @collections = params[:include_children] == "true" ? @user.accessible_collections : @user.collections
   end
 end
