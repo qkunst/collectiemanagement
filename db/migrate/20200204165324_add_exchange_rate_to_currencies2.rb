ca9cd728833fed17b5247c7a8af9d69b26cc5db4
murb <git@murb.nl>
2022-03-30 20:11:21 +0200

    separate updating data from migration of db

--- db/migrate/20200204165324_add_exchange_rate_to_currencies2.rb
+++ db/migrate/20200204165324_add_exchange_rate_to_currencies2.rb
@@ -4,8 +4,9 @@ class AddExchangeRateToCurrencies2 < ActiveRecord::Migration[5.2]
   def change
     {"USD" => 1.106270, "NLG" => 2.20371}.each do |code, rate|
       c = Currency.find_or_create_by(iso_4217_code: code)
-      c.exchange_rate = rate
       c.save
+      Currency.where(iso_4217_code: code).update_all(exchange_rate: rate)
+
     end
   end
 end
