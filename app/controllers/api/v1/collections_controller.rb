224d2c16a7cba9acf0f98366d6eea20efac8b821
murb <git@murb.nl>
2022-06-24 15:10:38 +0200

    do not rely on collections response

--- app/controllers/api/v1/collections_controller.rb
+++ app/controllers/api/v1/collections_controller.rb
@@ -1,7 +1,7 @@
 class Api::V1::CollectionsController < Api::V1::ApiController
   before_action :authenticate_activated_user!
   def index
-    @collections = params[:include_children] == "true" ? @user.accessible_collections : @user.collections
+    @collections = params[:include_children] == "true" ? @user.accessible_collections : Collection.for_user(@user).all
   end
 
   def show
