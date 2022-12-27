a6dab890eab334c160ed530e0b2c390e1ee07fdf
murb <git@murb.nl>
2022-12-27 16:58:14 +0100

    fix roundtrip issue

--- spec/features/json_export_import_roundtrip_spec.rb
+++ spec/features/json_export_import_roundtrip_spec.rb
@@ -44,8 +44,9 @@ RSpec.feature "Manage Collection", type: :feature do
       source_collection.works_including_child_works.include? works(:work1)
       expect(works(:work1).artists.length).to be > 0
       expect(works(:work1).time_spans.length).to be > 0
+      expect(works(:work1).alt_number_4).to eq("ALT4002123.1")
 
-      attributes_of_interest = %w[ title tag_list sources object_categories techniques damage_types frame_damage_types themes purchase_price_currency style cluster_name medium condition_work condition_frame subset placeability work_status owner_name ]
+      attributes_of_interest = %w[ alt_number_1 alt_number_4 title tag_list sources object_categories techniques damage_types frame_damage_types themes purchase_price_currency style cluster_name medium condition_work condition_frame subset placeability work_status owner_name ]
       string_compare_attributes_of_interest = %w[ created_at significantly_updated_at inventoried_at updated_at  time_spans ]
 
       work_pairs = collections(:collection1).works_including_child_works.pluck(:id).collect{|id| [Work.find(id), Work.find("99#{id}")]}
