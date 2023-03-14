757d2a93ffc2dbaf992d380ed9f262e686e658d1
murb <git@murb.nl>
2023-03-14 23:03:56 +0100

    use pupprb

--- app/workers/pdf_printer_worker.rb
+++ app/workers/pdf_printer_worker.rb
@@ -8,50 +8,16 @@ class PdfPrinterWorker
 
   sidekiq_options retry: true, backtrace: true, queue: :qkunst_quick
 
-  def clean_resource(url)
-    # urls are recognized as urls, but local files are not; simple trick that works on unixy systems
-    if /\A\/tmp\/[A-Za-z\d.\/]*/.match?(url)
-      "file://#{url}"
-    elsif /\A#{Rails.root}\/tmp\/[A-Za-z\d.\/]*/.match?(url)
-      "file://#{url}"
-    elsif url.start_with? File.join(Rails.root, "public")
-      "file://#{url}"
-    elsif url.start_with? "https://collectiemanagement.qkunst.nl/"
-      url
-    else
-      raise "Unsecure location (#{url})"
-    end
-  end
-
   def perform(url, options = {})
     inform_user_id = options[:inform_user_id] || options["inform_user_id"]
     subject_object_id = options[:subject_object_id] || options["subject_object_id"]
     subject_object_type = options[:subject_object_type] || options["subject_object_type"]
 
-    filename = Rails.root.join("tmp/#{SecureRandom.base58(32)}.pdf").to_s
-
-    resource = clean_resource(url)
+    filename = Pupprb::Pdf.write url
 
     # setting for debugging purposes
     @url = url
     @options = options
-
-    nvm_exec_location = "#{Dir.home}/.nvm/nvm-exec"
-
-    command = []
-    command += [nvm_exec_location] if File.exist?(nvm_exec_location)
-    command += [File.join(Rails.root, "bin", "puppeteer"), resource, filename]
-
-    env = {}
-    env["CHROME_DEVEL_SANDBOX"] = "/usr/local/sbin/chrome-devel-sandbox" if File.exist?("/usr/local/sbin/chrome-devel-sandbox")
-    env["CHROME_DEVEL_SANDBOX"] ||= "/usr/local/sbin/chrome_sandbox" if File.exist?("/usr/local/sbin/chrome_sandbox")
-
-    if !system("node --version")
-      raise "Node not found. Required."
-    end
-
-    Rails.logger.debug("Start creating a pdf using puppeteer, command: #{command.join(" ")}")
-    system(env, *command, exception: true)
 
     if inform_user_id
       Message.create(to_user_id: inform_user_id, subject_object_id: subject_object_id, subject_object_type: subject_object_type, from_user_name: "Download voorbereider", attachment: File.open(filename), message: "De download is gereed, open het bericht in je browser om de bijlage te downloaden.\n\nFormaat: PDF", subject: "PDF gereed")
