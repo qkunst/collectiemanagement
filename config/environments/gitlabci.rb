13f33c6118b6c095365b586fb871ea6d577931d1
murb <git@murb.nl>
2023-04-22 11:10:33 +0200

    make redis available again part 2

--- config/environments/gitlabci.rb
+++ config/environments/gitlabci.rb
@@ -68,3 +68,11 @@ OmniAuth.config.test_mode = true
 
 require "sidekiq/testing"
 Sidekiq::Testing.fake!
+
+Sidekiq.configure_server do |config|
+  config.redis = {url: "redis://redis:6379/0"}
+end
+
+Sidekiq.configure_client do |config|
+  config.redis = {url: "redis://redis:6379/0"}
+end
