fda0ff4c3aee129c2dacb951d5770cf8d8806976
murb <git@murb.nl>
2024-01-07 14:13:11 +0100

    allow vps601.directvps.nl

--- config/initializers/content_security_policy.rb
+++ config/initializers/content_security_policy.rb
@@ -11,7 +11,7 @@ Rails.application.configure do
     else
       policy.default_src :self
     end
-    policy.font_src :self
+    policy.font_src :self, "https://vps601.directvps.nl"
     policy.img_src :self, "https://stamen-tiles-a.a.ssl.fastly.net", "https://stamen-tiles-b.a.ssl.fastly.net", "https://stamen-tiles-c.a.ssl.fastly.net", "https://stamen-tiles-d.a.ssl.fastly.net", :data
     policy.object_src :none
     policy.script_src :self, "'unsafe-eval'", "'eval'", "'wasm-unsafe-eval'"
