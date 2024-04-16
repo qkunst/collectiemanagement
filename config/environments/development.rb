dd529f64f14631188beddbc96e41846e99e282e4
murb <git@murb.nl>
2024-04-16 15:58:39 +0200

    rspec: fix asset host

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -33,7 +33,7 @@ Rails.application.configure do
     config.cache_store = :null_store
   end
 
-  # config.action_controller.asset_host = "http://localhost:3000"
+  config.action_controller.asset_host = "http://localhost:3000"
   config.hosts << "collectiemanagement.test"
 
   # Store uploaded files on the local file system (see config/storage.yml for options).
