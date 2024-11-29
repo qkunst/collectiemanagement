3479d33347ecc61d32ff99adde3f693a89538c17
murb <git@murb.nl>
2024-11-29 20:52:18 +0100

    stabler sidekiq worker detection

--- app/controllers/status_controller.rb
+++ app/controllers/status_controller.rb
@@ -2,13 +2,17 @@
 
 class StatusController < ApplicationController
   skip_before_action :authenticate_activated_user!
+  layout false
 
   def application_status
     @sidekiq_running = Sidekiq::Workers.new.size > 0
     if @sidekiq_running == false
-      100.times { |t| TestWorker.perform_async(t / 50.0) }
-      sleep(2)
-      @sidekiq_running = Sidekiq::Workers.new.size > 0
+      before = sum_of_processed_and_in_process
+      TestWorker.perform_async(0.1)
+      30.times do |t|
+        next if (@sidekiq_running = (before < sum_of_processed_and_in_process))
+        sleep(1)
+      end
     end
     begin
       Collection.first.search_works("testsearch")[0]
@@ -16,5 +20,11 @@ class StatusController < ApplicationController
     rescue
       @elastic_search_running = false
     end
+  end
+
+  private
+
+  def sum_of_processed_and_in_process
+    (Sidekiq::Stats.new.processed + Sidekiq::Workers.new.size)
   end
 end
