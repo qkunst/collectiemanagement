9ee4ef8cc7c983f9adb000332e544808034d6471
murb <git@murb.nl>
2025-08-12 11:20:44 +0200

    spec fix

--- db/migrate/20240727074823_update_collection_caches.rb
+++ db/migrate/20240727074823_update_collection_caches.rb
@@ -1,14 +1,5 @@
 class UpdateCollectionCaches < ActiveRecord::Migration[7.1]
   def change
-    Work.reset_column_information # seeing errors related to removed columns
-
-    Collection.transaction do
-      Collection.all.each do |collection|
-        collection.cache_work_attributes_present!
-        collection.cache_derived_work_attributes_present!
-      end
-    rescue => e
-      p "Recaching failed: #{e.message}, but when you see this message you probably didn't have any data anyway :)"
-    end
+    # removed methods; this migration led to problems, when running from scratch there is nothing to update anyway.
   end
 end
